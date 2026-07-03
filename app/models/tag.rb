179a3112e4eb5a36ab249e7ba33747c917a8e294
Andrey Yurik <3swyylxo@anonaddy.com>
2026-07-03 12:31:06 +0300

    total refactoring

--- app/models/tag.rb
+++
@@ -1,8 +0,0 @@
-class Tag < ApplicationRecord
-  has_many :taggings, dependent: :destroy
-  has_many :essays,       through: :taggings, source: :taggable, source_type: "Essay"
-  has_many :builds,       through: :taggings, source: :taggable, source_type: "Build"
-  has_many :field_series, through: :taggings, source: :taggable, source_type: "FieldSeries"
-
-  validates :name, presence: true, uniqueness: true
-end
